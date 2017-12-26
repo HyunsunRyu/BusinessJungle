5f8cc576dd48dbe5f6580cb3856702406360db19
rhs <rhsgolpark@gmail.com>
2017-12-26 17:02:32 +0900

    commit

---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        IScene scene = GetScene();
+
+        scene.Init();
+    }
+
+    private IScene GetScene()
+    {
+        return GameObject.FindObjectOfType<IScene>();
+    }
+}
---
+++ Assets/Scripts/Scenes/GameScene/GameScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : IScene
+{
+    [SerializeField] private GameController gameController;
+    
+    public override void Init()
+    {
+        gameController.Init();
+    }
+}
